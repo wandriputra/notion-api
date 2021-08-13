3f4c3f1e2ff4910ee6efd0702e9f120b742ddd24
wandriputra <wandriputra@gmail.com>
2021-08-13 17:18:20 +0700

    update port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const axios = require('axios');
 const cors = require('cors')
 const app = express();
-const port = 5000;
+const port = 80;
 
 const SPLITBEEURL = 'https://notion-api.splitbee.io/v1/';
 
