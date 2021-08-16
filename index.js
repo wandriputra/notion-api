a1e9ea82e9a8aad32e75e54aff562d0ffb6221c0
wandriputra <wandriputra@gmail.com>
2021-08-16 20:12:31 +0700

    update

--- index.js
+++ index.js
@@ -24,11 +24,10 @@ const corsArr = {
 
 app.all('*', function (req, res, next) {
     let origin = req.headers.origin;
-    console.log(req.method)
     console.log(`${req.method} from : ` + origin);
 
     // if (corsArr.origin.indexOf(origin) >= 0) {
-    res.header("Access-Control-Allow-Origin", "https://mobifi.info");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Vary", "Origin");
     // res.header("Access-Control-Allow-Origin", "*");
     // }
