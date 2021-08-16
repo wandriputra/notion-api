ccfaa18bd7254593b4fc590148e762e9928e7915
wandriputra <wandriputra@gmail.com>
2021-08-16 13:22:51 +0700

    update

--- index.js
+++ index.js
@@ -6,31 +6,31 @@ const port = 80;
 
 const SPLITBEEURL = 'https://notion-api.splitbee.io/v1/';
 
-// var corsOptions = {
-//     origin: ["https://mobifi.io", "https://mobifi.info", "http://amazonaws.com"],
-//     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-// }
-
-const corsArr = {
-    origin: ["https://mobifi.io", "https://mobifi.info", "http://mobifi.info.s3-website.eu-west-1.amazonaws.com"]
+var corsOptions = {
+    origin: "https://mobifi.info",
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
+
+// const corsArr = {
+//     origin: ["https://mobifi.io", "https://mobifi.info", "http://mobifi.info.s3-website.eu-west-1.amazonaws.com"]
+// }
 
 app.use(express.json());
 app.use(express.urlencoded());
-// app.use(cors(corsOptions))
+app.use(cors(corsOptions))
 
-app.all('*', function (req, res, next) {
-    let origin = req.headers.origin;
-    if (corsArr.origin.indexOf(origin) >= 0) {
-        res.header("Access-Control-Allow-Origin", origin);
-    }
-    // if (req.method === 'OPTIONS') {
-    //     res.header('Access-Control-Allow-Methods', 'GET, HEAD, POST, PUT, DELETE, CONNECT, OPTIONS, TRACE, PATCH');
-    //     return res.status(200).json({});
-    // }
-    // res.header("Access-Control-Allow-Headers", "*, Accept, Content-Type, Content-Length, Accept-Encoding, CUSTOM-ALLOWED-HEADER");
-    next();
-});
+// app.all('*', function (req, res, next) {
+//     let origin = req.headers.origin;
+//     if (corsArr.origin.indexOf(origin) >= 0) {
+//         res.header("Access-Control-Allow-Origin", origin);
+//     }
+//     // if (req.method === 'OPTIONS') {
+//     //     res.header('Access-Control-Allow-Methods', 'GET, HEAD, POST, PUT, DELETE, CONNECT, OPTIONS, TRACE, PATCH');
+//     //     return res.status(200).json({});
+//     // }
+//     // res.header("Access-Control-Allow-Headers", "*, Accept, Content-Type, Content-Length, Accept-Encoding, CUSTOM-ALLOWED-HEADER");
+//     next();
+// });
 
 app.get('/:type/:id', async (req, res) => {
     const { params } = req;
