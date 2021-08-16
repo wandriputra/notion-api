f417eae588be75bb65964e4fdcbe435c0bb2f264
wandriputra <wandriputra@gmail.com>
2021-08-16 13:05:29 +0700

    update

--- index.js
+++ index.js
@@ -24,11 +24,11 @@ app.all('*', function (req, res, next) {
     if (corsArr.origin.indexOf(origin) >= 0) {
         res.header("Access-Control-Allow-Origin", origin);
     }
-    if (req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods', 'GET, HEAD, POST, PUT, DELETE, CONNECT, OPTIONS, TRACE, PATCH');
-        return res.status(200).json({});
-    }
-    res.header("Access-Control-Allow-Headers", "*, Accept, Content-Type, Content-Length, Accept-Encoding, CUSTOM-ALLOWED-HEADER");
+    // if (req.method === 'OPTIONS') {
+    //     res.header('Access-Control-Allow-Methods', 'GET, HEAD, POST, PUT, DELETE, CONNECT, OPTIONS, TRACE, PATCH');
+    //     return res.status(200).json({});
+    // }
+    // res.header("Access-Control-Allow-Headers", "*, Accept, Content-Type, Content-Length, Accept-Encoding, CUSTOM-ALLOWED-HEADER");
     next();
 });
 
