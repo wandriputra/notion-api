fe7f25d91594ac8944ab38bb97a820797c13c4eb
wandriputra <wandriputra@gmail.com>
2021-08-16 17:42:38 +0700

    update

--- index.js
+++ index.js
@@ -32,8 +32,8 @@ app.all('*', function (req, res, next) {
     res.header("Vary", "Origin");
     // res.header("Access-Control-Allow-Origin", "*");
     // }
-    if (req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods', 'GET, HEAD, POST, PUT, DELETE, CONNECT, OPTIONS, TRACE, PATCH');
+    res.header('Access-Control-Allow-Methods', '*');
+    if (req.method == 'OPTIONS') {
         return res.status(200).json({});
     }
     res.header("Access-Control-Allow-Headers", "*, Origin, X-Requested-With, Content-Type, Accept");
